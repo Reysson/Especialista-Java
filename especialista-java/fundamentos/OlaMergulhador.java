2bf298b406c20e2016e190420f5675b6f21dc342
Reysson Martins <reysson.martins30@gmail.com>
2022-06-24 11:39:56 -0400

    fundamentos - Variaveis

--- especialista-java/fundamentos/OlaMergulhador.java
+++ especialista-java/fundamentos/OlaMergulhador.java
@@ -6,7 +6,7 @@
 * */
 public class OlaMergulhador {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         //Não serei executado
 //        System.out.println("Olá, Mundo!");
     }
