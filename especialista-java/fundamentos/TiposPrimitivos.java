2042e15f38aef5457c042a629d952f8c2c20ecc6
Reysson Martins <reysson.martins30@gmail.com>
2022-06-27 17:20:43 -0400

    fundamentos - Tipos Primitivos: int e long

--- especialista-java/fundamentos/TiposPrimitivos.java
+++ especialista-java/fundamentos/TiposPrimitivos.java
@@ -13,11 +13,20 @@ public class TiposPrimitivos {
 
         //byte armazena valores de 0 a 127: Tamanho 8 bits (1 byte), Menor valor: -2^7, Maior valor: 2^7 - 1
         byte idade = 127; //valor máximo
-//      byte idade2 = 128; // não compila
+        //byte idade2 = 128; // não compila
 
-        //short armazena valores de 0 a 32767: Tamanho 8 bits (1 byte), Menor valor: -2^15, Maior valor: 2^15 - 1
+        //short armazena valores de 0 a 32767: Tamanho 16 bits (2 bytes), Menor valor: -2^15, Maior valor: 2^15 - 1
         short quantidadeEstoque = 32767; //valor máximo
-//      short quantidadeEstoque2 = 32768; //não compila
+        //short quantidadeEstoque2 = 32768; //não compila
+
+        //int armazena valores inteiros de 0 2147483648: Tamanho 32 bits (4 bytes), Menor valor: -2^31, Maior valor: 2^31 - 1
+        int populacaoUberlandia = 699_097;
+        //int populacaoUberlandia2 = 2_147_483_648; não compila
+
+        //long armazena valores inteiros de 0 9,223372037×10¹⁸: Tamanho 64 bits (8 bytes), Menor valor: -2^63, Maior valor: 2^63 - 1
+        long meuDinheiro = 515151484231212115618484515689486;
+
+
 
     }
 }
