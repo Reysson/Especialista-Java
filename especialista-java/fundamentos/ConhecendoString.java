ba43307c945dd065c3c809672121a51000b50a6c
Reysson Martins <reysson.martins30@gmail.com>
2022-06-28 10:15:34 -0400

    fundamentos - Strings

---
+++ especialista-java/fundamentos/ConhecendoString.java
@@ -0,0 +1,9 @@
+public class ConhecendoString {
+
+    public static void main(String[] args) {
+        String nome = "José";
+        String idade = "20";
+
+        System.out.println(nome + " tem " + idade + " anos");
+    }
+}
