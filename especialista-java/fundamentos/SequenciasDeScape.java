c3094bd8d7b6ad9b579cd0e54279e64ba2ea5447
Reysson Martins <reysson.martins30@gmail.com>
2022-06-28 10:25:08 -0400

    fundamentos - Sequencia de Scape

---
+++ especialista-java/fundamentos/SequenciasDeScape.java
@@ -0,0 +1,14 @@
+public class SequenciasDeScape {
+
+    public static void main(String[] args) {
+
+        //Escapando aspas dentro de string
+        System.out.println("nome \"Maria\"");
+
+        //Pulando linha
+        System.out.println("nome e idade: \nJose 20 anos");
+
+        //Escando \
+        System.out.println("C:\\Windows");
+    }
+}
